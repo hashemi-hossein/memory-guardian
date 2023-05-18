6292c382f1a2113c5b8efa388dd7acc13f8d35b4
Hossein Hashemi <hashemi@tutamail.com>
2023-05-18 11:06:52 +0330

    add clear button to clear manually and a switch to change auto clearing state

--- app/src/main/java/ara/memoryguardian/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/ara/memoryguardian/ui/screen/home/HomeScreen.kt
@@ -1,8 +1,46 @@
 package ara.memoryguardian.ui.screen.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen() {
+fun HomeScreen(viewModel: HomeViewModel = viewModel()) {
+    val uiState by viewModel.uiState.collectAsState()
 
+    Scaffold { contentPadding ->
+        Box(
+            modifier = Modifier
+                .padding(contentPadding)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center,
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+            ) {
+                Button(onClick = viewModel::clearClipBoard) {
+                    Text(text = "Clear Clipboard")
+                }
+
+                Text(text = "Auto Clear Clipboard")
+                Switch(checked = uiState.enabled, onCheckedChange = {})
+            }
+        }
+    }
 }
