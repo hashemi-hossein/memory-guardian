7c10563a8b7be704b3fb163c30ba39fd642b6d98
Hossein Hashemi <hashemi@tutamail.com>
2023-05-18 11:02:05 +0330

    add lifecycle-viewmodel-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,4 +64,6 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 }
