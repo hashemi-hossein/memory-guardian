4c935744b5e15dd34b8dbf0dd9be4dcd1cbe725d
Hossein Hashemi <hashemi@tutamail.com>
2023-05-18 13:50:17 +0330

    modify Main.kt to use hiltViewModel function

--- app/src/main/java/ara/memoryguardian/ui/main/Main.kt
+++ app/src/main/java/ara/memoryguardian/ui/main/Main.kt
@@ -1,12 +1,15 @@
 package ara.memoryguardian.ui.main
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import ara.memoryguardian.ui.screen.home.HomeScreen
+import ara.memoryguardian.ui.screen.home.HomeViewModel
 import ara.memoryguardian.ui.theme.MemoryGuardianTheme
 
 @Composable
 fun Main() {
     MemoryGuardianTheme {
-        HomeScreen()
+        val viewModel = hiltViewModel<HomeViewModel>()
+        HomeScreen(viewModel = viewModel)
     }
 }
