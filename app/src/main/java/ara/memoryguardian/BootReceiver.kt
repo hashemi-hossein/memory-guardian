c63edb96811ed10d4d77835c90fe87b0f6a0dc05
Hossein Hashemi <hashemi@tutamail.com>
2023-05-19 19:18:43 +0330

    add BootReceiver.kt to reschedule WorkManager

---
+++ app/src/main/java/ara/memoryguardian/BootReceiver.kt
@@ -0,0 +1,39 @@
+package ara.memoryguardian
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import ara.memoryguardian.work.HClipboard
+import ara.note.domain.usecase.userpreferences.ReadUserPreferencesUseCase
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class BootReceiver : BroadcastReceiver() {
+
+    @Inject
+    lateinit var readUserPreferencesUseCase: ReadUserPreferencesUseCase
+
+    @Inject
+    lateinit var hClipboard: HClipboard
+
+    private val job: Job = Job()
+    private val coroutineScope = CoroutineScope(job + Dispatchers.IO)
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Timber.d("BootReceiver -- onReceive -- intent?.action=${intent?.action}")
+
+        if (context != null && intent?.action == Intent.ACTION_BOOT_COMPLETED) {
+            coroutineScope.launch {
+                if (readUserPreferencesUseCase().isAutoCleaningEnable) {
+                    hClipboard.toggleAutoClearing(true)
+                }
+            }
+        }
+    }
+}
