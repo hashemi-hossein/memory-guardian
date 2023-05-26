e784250003da4ded01efba77e98947d2cc8f60b4
Hossein Hashemi <hashemi@tutamail.com>
2023-05-26 08:57:46 +0330

    add notification switch to toggle showing notifications and get the runtime permission on Android version 13+

--- app/src/main/java/ara/memoryguardian/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/ara/memoryguardian/ui/screen/home/HomeScreen.kt
@@ -1,5 +1,10 @@
 package ara.memoryguardian.ui.screen.home
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,15 +22,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = viewModel()) {
     val uiState by viewModel.uiState.collectAsState()
+    val context = LocalContext.current
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
 
     Scaffold { contentPadding ->
         Box(
@@ -57,6 +71,36 @@ fun HomeScreen(viewModel: HomeViewModel = viewModel()) {
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                     label = { Text(text = "Interval") },
                 )
+
+                val notificationPermissionRequest = rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+                    if (isGranted) {
+                        viewModel.toggleNotification(true)
+                    } else {
+                        Timber.e("POST_NOTIFICATIONS permission have not granted")
+                        viewModel.showSnackbar("The Permission is needed to show Notifications")
+                    }
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(10.dp),
+                ) {
+                    Text(text = "Notification")
+                    Switch(
+                        checked = uiState.isNotificationEnable,
+                        onCheckedChange = {
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+                                (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS)
+                                        != PackageManager.PERMISSION_GRANTED)
+                            ) {
+                                Timber.d("launch notification permission requester")
+                                notificationPermissionRequest.launch(Manifest.permission.POST_NOTIFICATIONS)
+                            } else {
+                                viewModel.toggleNotification(it)
+                            }
+                        }
+                    )
+                }
             }
         }
     }
--- app/src/main/java/ara/memoryguardian/ui/screen/home/HomeState.kt
+++ app/src/main/java/ara/memoryguardian/ui/screen/home/HomeState.kt
@@ -1,6 +1,8 @@
 package ara.memoryguardian.ui.screen.home
 
 data class HomeState(
+    val snackbarMessage: String? = null,
+    val isNotificationEnable: Boolean = false,
     val isAutoCleaningEnable: Boolean = false,
     val autoCleaningInterval: String = "15",
 )
