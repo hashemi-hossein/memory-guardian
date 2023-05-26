6f45502188b4e4e1950cad9dd663acb2c112f586
Hossein Hashemi <hashemi@tutamail.com>
2023-05-26 09:28:00 +0330

    Release Version 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 21
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
