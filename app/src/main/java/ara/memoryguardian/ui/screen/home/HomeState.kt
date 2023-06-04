38f77c933840f153b7da67d79e1b5f28156d02a1
Hossein Hashemi <hashemi@tutamail.com>
2023-06-04 12:11:42 +0330

    add clipboardContent property to HomeState

--- app/src/main/java/ara/memoryguardian/ui/screen/home/HomeState.kt
+++ app/src/main/java/ara/memoryguardian/ui/screen/home/HomeState.kt
@@ -4,7 +4,10 @@ import ara.memoryguardian.DEFAULT_AUTO_CLEANING_INTERVAL
 
 data class HomeState(
     val snackbarMessage: String? = null,
+
     val isNotificationEnable: Boolean = false,
     val isAutoCleaningEnable: Boolean = false,
     val autoCleaningInterval: String = DEFAULT_AUTO_CLEANING_INTERVAL,
+
+    val clipboardContent: String = "",
 )
