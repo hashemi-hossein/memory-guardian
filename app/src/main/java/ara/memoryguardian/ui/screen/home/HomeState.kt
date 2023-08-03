eba63bad1b4cc838c433463aca77d134ae7883ff
Hossein Hashemi <hashemi@tutamail.com>
2023-08-03 11:31:13 +0330

    add isSmallPopupEnable property to HomeState.kt

--- app/src/main/java/ara/memoryguardian/ui/screen/home/HomeState.kt
+++ app/src/main/java/ara/memoryguardian/ui/screen/home/HomeState.kt
@@ -6,6 +6,7 @@ data class HomeState(
     val snackbarMessage: String? = null,
 
     val isNotificationEnable: Boolean = false,
+    val isSmallPopupEnable: Boolean = false,
     val isAutoCleaningEnable: Boolean = false,
     val autoCleaningInterval: String = DEFAULT_AUTO_CLEANING_INTERVAL,
 
