c69e7c0828839c75c0c45fb335321495a4e90dfa
Hossein Hashemi <hossein.hashemi@elastique.nl>
2024-03-16 18:28:52 +0100

    Release Version 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "ara.memoryguardian"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
