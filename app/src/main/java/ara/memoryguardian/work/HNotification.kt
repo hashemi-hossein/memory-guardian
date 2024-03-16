315c8b2d4bd8c227b1d4f337c3f858d351230d36
Hossein Hashemi <hossein.hashemi@elastique.nl>
2024-03-16 18:13:34 +0100

    change notification icon

--- app/src/main/java/ara/memoryguardian/work/HNotification.kt
+++ app/src/main/java/ara/memoryguardian/work/HNotification.kt
@@ -25,7 +25,7 @@ fun Context.showNotification(title: String, message: String) {
     val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
     val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
-        .setSmallIcon(R.drawable.ic_launcher_foreground)
+        .setSmallIcon(R.drawable.ic_clipboard)
         .setContentTitle(title)
         .setContentText(message)
         .setPriority(NotificationCompat.PRIORITY_DEFAULT)
