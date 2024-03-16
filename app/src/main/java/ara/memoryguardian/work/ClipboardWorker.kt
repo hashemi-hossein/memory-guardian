c85b61e86322db85b91ebc2cb9405b41228ab783
Hossein Hashemi <hossein.hashemi@elastique.nl>
2024-03-16 18:16:01 +0100

    run reformat

--- app/src/main/java/ara/memoryguardian/work/ClipboardWorker.kt
+++ app/src/main/java/ara/memoryguardian/work/ClipboardWorker.kt
@@ -20,11 +20,11 @@ class ClipboardWorker(
         val clipboardManager = context.getClipboardManager()
         clipboardManager.clear()
 
-        val isNotificationEnable = inputData.getBoolean("isNotificationEnable",false)
+        val isNotificationEnable = inputData.getBoolean("isNotificationEnable", false)
         if (isNotificationEnable)
             context.showNotification(context.getString(R.string.clipboard_cleared), "")
 
-        val isSmallPopupEnable = inputData.getBoolean("isSmallPopupEnable",false)
+        val isSmallPopupEnable = inputData.getBoolean("isSmallPopupEnable", false)
         if (isSmallPopupEnable)
             Toast.makeText(context, context.getString(R.string.clipboard_cleared), Toast.LENGTH_LONG).show()
 
