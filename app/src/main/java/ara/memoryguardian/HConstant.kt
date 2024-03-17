19b83f038a14b2ff518fdd4b2b0c3c6d41626895
Hossein Hashemi <hossein.hashemi@elastique.nl>
2024-03-17 05:18:25 +0100

    differentiate notification ids

--- app/src/main/java/ara/memoryguardian/HConstant.kt
+++ app/src/main/java/ara/memoryguardian/HConstant.kt
@@ -3,6 +3,6 @@ package ara.memoryguardian
 const val DEBUG = true
 const val DEFAULT_AUTO_CLEANING_INTERVAL = "60"
 
-const val FOREGROUND_SERVICE_NOTIFICATION_ID = 2531
+const val FOREGROUND_SERVICE_NOTIFICATION_ID = 2532
 const val FOREGROUND_SERVICE_NOTIFICATION_CHANNEL_ID = "ForegroundServiceChannel"
 const val FOREGROUND_SERVICE_NOTIFICATION_CHANNEL_NAME = "Foreground Service Channel"
