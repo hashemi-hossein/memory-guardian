cbcd4ba8fb9fcd444e36f6ebefae4a7887492181
Hossein Hashemi <91213707+hashemi-hossein@users.noreply.github.com>
2024-07-21 11:38:00 +0200

    remove @Suppress("DSL_SCOPE_VIOLATION") from top level build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.hilt.android) apply false
 }
-true // Needed to make the Suppress annotation work for the plugins block
